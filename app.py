aa2107eec0c162fe93af48669188b69404e54493
Suyog Acharya <asuyog042@gmail.com>
2024-05-21 16:14:32 +0545

    pie chart added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 from db import DB
 
 db= DB()
@@ -11,11 +12,27 @@ if user_option == 'Check Flights':
     col1, col2 = st.columns(2)
     city=db.fetch_city_names()  
     with  col1:
-        st.selectbox('Select Source City', sorted(city))
+        source=st.selectbox('Select Source City', sorted(city))
     with col2:
-        st.selectbox('Select Destination City', sorted(city))
+        destination=st.selectbox('Select Destination City', sorted(city))
+    if st.button('Search'):
+        results= db.fetch_all_flights(source, destination)
+        st.dataframe(results)
 elif user_option == 'Analytics':
     st.title('Welcome to Flights Analytics')
+    airline, frequency = db.fetch_airline_frequency()
+    fig= go.Figure(
+        go.Pie(
+            labels= airline,
+            values=frequency,
+            hoverinfo="label+percent",
+            textinfo="value"
+        )
+    )
+    st.header("Pie Chart")
+    st.plotly_chart(fig)
+
+
 else:
     pass
 
--- db.py
+++ db.py
@@ -28,6 +28,31 @@ class DB:
             city.append(item[0])
         
         return city
+    
+    def fetch_all_flights(self,source,destination):
+        self.cursor.execute('''
+        select Airline, Route, Dep_Time, Duration, Price From flights
+        where Source = '{}' and destination ='{}'
+
+        '''.format(source, destination))
+        data = self.cursor.fetchall()
+        return data
+
+    def fetch_airline_frequency(self):
+        airline= []
+        frequency=[]
+        self.cursor.execute(
+            '''
+            select Airline, COUNT(*) from flights
+            group by Airline
+            '''
+        )
+        data =self.cursor.fetchall()
+        for item in data:
+            airline.append(item[0])
+            frequency.append(item[1])
+
+        return airline, frequency
 
         
 
